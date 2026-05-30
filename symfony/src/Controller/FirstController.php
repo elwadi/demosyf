ea92ac44e87430717cb3af051339a9de3e7b9cf5
Amine <elwadiamine@gmail.com>
2026-05-30 16:20:27 +0100

    add amqp

--- symfony/src/Controller/FirstController.php
+++ symfony/src/Controller/FirstController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Message\Tracking;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Messenger\MessageBusInterface;
+
 
 final class FirstController extends AbstractController
 {
@@ -25,5 +28,15 @@ final class FirstController extends AbstractController
         return $this->render('first/index.html.twig', [
             'controller_name' => 'FirstController',
         ]);
+    }
+    
+    #[Route('/messages', name: 'app_message')]
+    public function mymessages(MessageBusInterface $bus): Response
+    {
+        for($i=0;$i<100;$i++){
+        $bus->dispatch(new Tracking("Trackin #".$i));
+
+        }
+        return new Response('Ok messages');
     }
 }
